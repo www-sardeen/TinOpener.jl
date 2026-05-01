9e324b62087ff554ca87b054c4fb752c859399e5
hkauhanen <nenahuak@gmail.com>
2026-05-01 10:40:13 +0200

    fixed bug in colvolve()

--- src/convolutions.jl
+++ src/convolutions.jl
@@ -10,7 +10,7 @@ In other words, compute the quantity
 (K \\star x)_t = \\sum_{s=0}^S K(s) x(t-s).
 ```
 """
-function convolve(x::Vector{Float64}, t::Int, K::Function, M::Int)
+function convolve(x::Vector{Float64}, t::Int, K::Function, S::Int)
     sum([K(s) * x[t - s] for s in 0:S])
 end
 
